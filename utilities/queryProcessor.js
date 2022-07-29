2386bd0ae1d4f0c465605b23df3a04e49076dd6c
Devesh Patel <deveshpatel13599@gmail.com>
2022-07-29 18:44:09 -0400

    optimize query preprocessor

--- utilities/queryProcessor.js
+++ utilities/queryProcessor.js
@@ -1,19 +1,10 @@
 const moment = require('moment');
 
 module.exports.processQuery = (query) => {
-    if (query.perPage) {
-        query['limit'] = Number.parseInt(query.perPage);
-    } else {
-        query['limit'] = 10;
-    }
+    query['limit'] = query.perPage
     delete query['perPage'];
 
-    if (query.page) {
-        query.page = query.page === '0' ? '1' : query.page;
-        query['skip'] = (Number.parseInt(query.page) - 1) * query.limit;
-    } else {
-        query['skip'] = 0;
-    }
+    query['skip'] = (query.page - 1) * query.limit;
     delete query['page'];
 
     if (query['fromDate']) {
@@ -26,21 +17,5 @@ module.exports.processQuery = (query) => {
         query['toDate'] = moment
             .utc(query.toDate)
             .format();
-    }
-
-    if (query['minAmount']) {
-        query['minAmount'] = Number.parseFloat(query['minAmount']);
-    } else {
-        query['minAmount'] = 0;
-    }
-
-    if (query['maxAmount']) {
-        query['maxAmount'] = Number.parseFloat(query['maxAmount']);
-    } else {
-        query['maxAmount'] = Number.MAX_SAFE_INTEGER;
-    }
-
-    if (query.tagId) {
-        query['tagId'] = query.tagId;
     }
 };
